b86eebc2734d82cbf758958e6e27e2c8a77f81be
Vikas singh varma <Vikashverma3001@gmail.com>
2024-01-01 19:51:26 +0530

    add sign up functionalities

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,8 +1,24 @@
 import { asyncHandler } from "../utils/asyncHandler";
-
+import {User} from '../model/user.model.js'
 // user sing up 
 
 const signUp  = asyncHandler( async(req,res)=>{
+    try {
+        const user = await User.create(req.body);
+        return res
+        .status(200)
+        .json({
+            user,
+            message: "user sign-Up successfully !!"
+        })
+    } catch (error) {
+        return res
+        .status(501)
+        .json({
+            message: error.message
+        })
+
+    }
 
 })
 
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,9 @@
 import express from 'express'
 import { signIn, signUp } from '../controllers/user.controller'
-
+import {signUpPageValidator} from '../middlewares/signUpPageValidator.js'
 const router = express.Router()
 
-router.route('/register').post(signUp)
+router.route('/register').post(signUpPageValidator,signUp)
 router.route('/register').post(signIn)
 
 export default router
