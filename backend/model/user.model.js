4e8ab0717f6b25deff9bf89905594fc5a737804b
Vikas singh varma <Vikashverma3001@gmail.com>
2024-01-02 13:17:58 +0530

    remove all the code and start with fresh

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -1,66 +1,67 @@
-import monogoose from 'mongoose'
-import JWT from 'jsonwebtoken'
+import mongoose from 'mongoose'
 import bcrypt from 'bcrypt'
-
-const userSchema = await monogoose.Schema({
-    fullName:{
+import JWT from 'jsonwebtoken'
+const userSchema = new mongoose.Schema({
+    username:{
         type: String,
-        required: true,
-        trim: true,
-        minLength: [5, "Name must be at least 5 char"],
-    },
-    username: {
-        type: String,
-        required: true,
-        trim: true,
-        unique: true,
+        required: [true, "username required"],
+        unique: [true, "username must be unique"],
         lowercase: true,
+        trim: true,
+        index: true
     },
     email:{
         type: String,
-        required: true,
-        trim: true,
+        required: [true, "Email must be required"],
         unique: true,
         lowercase: true,
+        trim: true,
     },
-    password:{
+    fullName:{
         type: String,
-        required: [true, 'Password is required'],
+        required: [true, "Name must be required"],
+        trim: true,
+        index: true
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required']
     },
     bio:{
-        type: String,
+        type: String
     }
-}, { timestamps: true
-})
+}, {timestamps: true})
 
-// generate token 
-userSchema.methos.generateAuthToken = {
-    function(){
-        return JWT.sign({
-            _id: this.id,
-            email: this.email,
-            username: this.username
-        },
 
-        process.env.ACCESS_TOKEN_SECRET,
-
-        {
-            expiresIn: '24h'
-        }
-        )
-    }
-}
-
-// hash password
 userSchema.pre("save", async function(next){
+    // check the password is modifeid or not
     if(!this.isModified('password')) return next()
+
     try {
         this.password = await bcrypt.hash(this.password, 10)
         next()
-    } catch (error) {
+    } catch ({error}) {
         console.log(error.message)
-        next(error);
+        next(error)
     }
 })
 
-export const User = monogoose.model("User", userSchema);
+//password check
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password)
+}
+
+// generate access token
+userSchema.methods.generateAccessToken = function(){
+    return JWT.sign(
+        {
+        _id: this.id,
+        username: this.username,
+        email: this.email,
+    },
+    process.env.ACCESS_TOKEN_SECRET,
+    {
+        expiresIn: '24h'
+    }
+)}
+export const User = mongoose.model("User", userSchema);
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,6 @@
 import express from 'express'
-import { signIn, signUp } from '../controllers/user.controller'
-import { signInPageValidator, signUpPageValidator } from '../middlewares/user.middlewares.js'
 const router = express.Router()
 
-router.route('/register').post(signUpPageValidator,signUp)
-router.route('/register').post(signInPageValidator,signIn)
+
 
 export default router
